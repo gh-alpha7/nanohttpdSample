8921859a928f97419741dbb9270610d5277e0bd2
subh1685 <shubhamsngh067@gmail.com>
2020-12-24 10:41:41 +0530

    upload file

--- app/src/main/java/com/example/nanohttpdtestapp/MainActivity.java
+++ app/src/main/java/com/example/nanohttpdtestapp/MainActivity.java
@@ -15,7 +15,12 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
 
@@ -47,15 +52,46 @@ public class MainActivity extends AppCompatActivity {
         public Response serve(IHTTPSession session) {
             String uri = session.getUri();
             FileInputStream fis = null;
-            String fileName = Environment.getExternalStorageDirectory()
-                    + "/nanofile.txt";
-            File file = new File(fileName);
-            try {
-                fis = new FileInputStream(file);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
+            Map<String, String> files = new HashMap<>();
+            if("/get".equalsIgnoreCase(uri)) {
+                String fileName = Environment.getExternalStorageDirectory()
+                        + "/nanofile.txt";
+                File file = new File(fileName);
+                try {
+                    fis = new FileInputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                return newFixedLengthResponse(Response.Status.OK, "text/plain", fis, file.length());
             }
-            return newFixedLengthResponse(Response.Status.OK, "text/plain", fis, file.length());
+            else{
+                try {
+                    session.parseBody(files);
+                    File dst = new File(Environment.getExternalStorageDirectory()+"/nanoupload.txt");
+                    File src = new File(files.get("myFile"));
+                    try {
+                        copy(src, dst);
+                    }catch (Exception e){ e.printStackTrace();}
+                }
+                catch (Exception e){
+
+                }
+                return newFixedLengthResponse("{}");
+            }
+        }
+
+        public void copy(File src, File dst) throws IOException {
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dst);
+
+            // Transfer bytes from in to out
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
         }
     }
 }
