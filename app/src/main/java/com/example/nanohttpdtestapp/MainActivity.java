637e0af769703d7af96799af0a25e0591a49e8fb
subh1685 <shubhamsngh067@gmail.com>
2020-12-29 11:58:15 +0530

    add toast

--- app/src/main/java/com/example/nanohttpdtestapp/MainActivity.java
+++ app/src/main/java/com/example/nanohttpdtestapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -27,14 +28,14 @@ import fi.iki.elonen.NanoHTTPD;
 
 public class MainActivity extends AppCompatActivity {
     private WebServer server;
-
+    public Activity currActivity;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        currActivity = this;
         server = new WebServer(8080);
         try {
             server.start();
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     class WebServer extends NanoHTTPD{
+        public int requestCount = 0;
         public WebServer(int port){
             super(port);
         }
@@ -53,6 +55,13 @@ public class MainActivity extends AppCompatActivity {
             String uri = session.getUri();
             FileInputStream fis = null;
             Map<String, String> files = new HashMap<>();
+            requestCount += 1;
+            currActivity.runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(currActivity, "Got new request "+ requestCount, Toast.LENGTH_SHORT).show();
+                }
+            });
+
             if("/get".equalsIgnoreCase(uri)) {
                 String fileName = Environment.getExternalStorageDirectory()
                         + "/nanofile.txt";
@@ -62,7 +71,8 @@ public class MainActivity extends AppCompatActivity {
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
                 }
-                return newFixedLengthResponse(Response.Status.OK, "application/octet-stream", fis, file.length());
+//                return newFixedLengthResponse(Response.Status.OK, "application/octet-stream", fis, file.length());
+                return newFixedLengthResponse(Response.Status.OK, "text/plain", fis, file.length());
             }
             else{
                 try {
